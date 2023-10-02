f26c4412a012af3f908d206e25f09cd1313edd64
LAPTOP-3Q2E57HJ\juanc <1105363626@u.icesi.edu.co>
2023-10-01 22:39:48 -0500

    Queue and Stack tests implementation

---
+++ test/collections/QueueTest.java
@@ -0,0 +1,254 @@
+package collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import collections.dataStructures.Queue;
+import collections.interfaces.IQueue;
+
+import exception.QueueException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class QueueTest {
+
+    private IQueue<String> queue;
+
+    @BeforeEach
+    void setUp() {
+        queue = new Queue<String>();
+    }
+
+    @Test
+    void testEnqueueOneElement() {
+        // Arrange
+        String item = "item";
+        String element = "";
+        boolean exceptionThrown = false;
+
+        // Act
+        try {
+            queue.enqueue(item);
+            element = queue.dequeue();
+        } catch (QueueException e) {
+            exceptionThrown = true;
+        }
+
+        // Assert
+        assertFalse(exceptionThrown);
+        assertFalse(queue.isEmpty());
+        assertEquals(item, element);
+    }
+
+    @Test
+    void testEnqueueTwoElements() {
+        // Arrange
+        String item1 = "item1";
+        String item2 = "item2";
+        String element1 = "";
+        String element2 = "";
+        boolean exceptionThrown = false;
+
+        // Act
+        try {
+            queue.enqueue(item1);
+            queue.enqueue(item2);
+            element1 = queue.dequeue();
+            element2 = queue.dequeue();
+        } catch (QueueException e) {
+            exceptionThrown = true;
+        }
+
+        // Assert
+        assertFalse(exceptionThrown);
+        assertEquals(item1, element1);
+        assertEquals(item2, element2);
+    }
+
+    @Test
+    void testEnqueueOneElementGetsOnFront() {
+        // Arrange
+        String item1 = "item2";
+        String item2 = "item1";
+        String front = "";
+        boolean exceptionThrown = false;
+
+        // Act
+        try{
+            queue.enqueue(item1);
+            queue.enqueue(item2);
+            front = queue.front();
+        }catch (QueueException e){
+            exceptionThrown = true;
+        }
+
+        // Assert
+        assertFalse(exceptionThrown);
+        assertEquals(item1, front);
+        assertNotEquals(item2, front);
+    }
+
+    @Test
+    void testFrontEmptyQueue() {
+        // Arrange
+        boolean exceptionThrown = false;
+
+        // Act
+        try{
+            queue.front();
+        }catch (QueueException e){
+            exceptionThrown = true;
+        }
+
+        // Assert
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    void testFrontNonEmpty() {
+        // Arrange
+        String item1 = "item1";
+        String front = "";
+        boolean exceptionThrown = false;
+
+        // Act
+        try {
+            queue.enqueue(item1);
+            front = queue.front();
+        } catch (QueueException e) {
+            exceptionThrown = true;
+        }
+
+        // Assert
+        assertFalse(exceptionThrown);
+        assertEquals(item1, front);
+    }
+
+    @Test
+    void testFrontAfterDequeue() {
+        // Arrange
+        // Arrange
+        String item1 = "item1";
+        String front = "";
+        boolean exceptionThrown = false;
+
+        // Act
+        try {
+            queue.enqueue(item1);
+            queue.dequeue();
+            front = queue.front();
+        } catch (QueueException e) {
+            exceptionThrown = true;
+        }
+
+        // Assert
+        assertTrue(exceptionThrown);
+        assertNotEquals(item1, front);
+    }
+
+    @Test
+    void testDequeueWhenCreated() {
+        // Act
+        boolean exceptionThrown = false;
+
+        // Assert
+        try {
+            queue.dequeue();
+        } catch (QueueException e) {
+            exceptionThrown = true;
+        }
+
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    void testDequeueWhenOneElements() {
+        // Arrange
+        String item1 = "item1";
+        String dequeue = "";
+        boolean exceptionThrown = false;
+
+        // Act
+        try {
+            queue.enqueue(item1);
+            dequeue = queue.dequeue();
+        } catch (QueueException e) {
+            exceptionThrown = true;
+        }
+
+        // Assert
+        assertFalse(exceptionThrown);
+        assertEquals(item1, dequeue);
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    void testDequeueWhenMultipleElements() {
+        // Arrange
+        String item1 = "item1";
+        String item2 = "item2";
+        String item3 = "item3";
+        String dequeue1 = "";
+        String dequeue2 = "";
+        boolean exceptionThrown = false;
+
+        // Act
+        try {
+            queue.enqueue(item1);
+            queue.enqueue(item2);
+            queue.enqueue(item3);
+            dequeue1 = queue.dequeue();
+            dequeue2 = queue.dequeue();
+        }catch (QueueException e){
+            exceptionThrown = true;
+        }
+
+        // Assert
+        assertFalse(exceptionThrown);
+        assertEquals(item3, dequeue1);
+        assertEquals(item2, dequeue2);
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    void testIsEmptyAfterCreation() {
+        // Act - Assert
+        assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    void testIsEmptyAfterEnqueue() {
+        // Arrange
+        String item = "item1";
+        boolean exceptionThrown = false;
+
+        try {
+            queue.enqueue(item);
+        }catch (QueueException e){
+            exceptionThrown = true;
+        }
+
+        // Act - Assert
+        assertFalse(exceptionThrown);
+        assertFalse(queue.isEmpty());
+    }
+
+    @Test
+    void testIsEmptyAfterDequeuMultipleElements() {
+        // Arrange
+        String item1 = "item1";
+        String item2 = "item2";
+        boolean exceptionThrown = false;
+
+        try {
+            queue.enqueue(item1);
+            queue.enqueue(item2);
+            queue.dequeue();
+            queue.dequeue();
+        }catch (QueueException e){
+            exceptionThrown = true;
+        }
+
+        // Act - Assert
+        assertFalse(exceptionThrown);
+        assertTrue(queue.isEmpty());
+    }
+}
---
+++ test/collections/StackTest.java
@@ -0,0 +1,194 @@
+package collections;
+
+import collections.dataStructures.Stack;
+import collections.interfaces.IStack;
+
+import exception.StackException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StackTest {
+
+    private IStack<String> stack;
+
+    public void setUp1() {
+        stack = new Stack<String>();
+    }
+
+    public void setUp2() {
+        stack = new Stack<String>();
+        try {
+            stack.push("item");
+        } catch (Exception e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+    }
+    // Test methods for size() method
+    @Test
+    public void testSizeEmpty() {
+        //setup
+        setUp1();
+
+        // act
+        int size = stack.size();
+
+        // assert
+        assertEquals(0, size);
+    }
+
+    @Test
+    public void testSizeFilled() {
+        // setup
+        setUp2();
+
+        // act & assert
+        assertEquals(1, stack.size());
+    }
+
+    // Test methods for isEmpty() method
+    @Test
+    public void testIsEmptyEmpty() {
+        // setup
+        setUp1();
+
+        // act & assert
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testIsEmptyFilled() {
+        // setup
+        setUp2();
+
+        // act & assert
+        assertFalse(stack.isEmpty());
+    }
+
+    // Test methods for push(E item) method
+
+    @Test
+    public void testPushEmpty() {
+        // setup
+        setUp1();
+        boolean isExceptionThrow = false;
+        String top = "";
+
+        // act
+        try {
+            stack.push("item");
+            top = stack.top();
+        }catch (StackException e){
+            isExceptionThrow = true;
+        }
+        // assert
+        assertFalse(isExceptionThrow);
+        assertEquals(1, stack.size());
+        assertEquals("item", top);
+    }
+
+    @Test
+    public void testPushFilled() {
+        // setup
+        setUp1();
+        boolean isExceptionThrow = false;
+        String top = "";
+        int size = stack.size();
+
+        // act
+        try {
+            stack.push("item");
+        }catch (StackException e){
+            isExceptionThrow = true;
+        }
+        // assert
+        assertFalse(isExceptionThrow);
+        assertEquals(size+1, stack.size());
+        assertEquals("item", top);
+    }
+
+    @Test
+    public void testPushNull(){
+        // setup
+        setUp1();
+
+        // act & assert
+        assertThrows(StackException.class, () -> stack.push(null));
+    }
+
+    // Test methods for top() method
+
+    @Test
+    public void testTopEmpty() {
+        // setup
+        setUp1();
+        boolean isExceptionThrow = false;
+        int size = stack.size();
+
+        // act
+        try {
+            stack.top();
+        }catch (StackException e){
+            isExceptionThrow = true;
+        }
+        // assert
+        assertTrue(isExceptionThrow);
+    }
+
+    @Test
+    public void testTopFilled() {
+        // setup
+        setUp1();
+        boolean isExceptionThrow = false;
+        String top = "";
+        int size = stack.size();
+
+        // act
+        try {
+            top = stack.top();
+        }catch (StackException e){
+            isExceptionThrow = true;
+        }
+        // assert
+        assertFalse(isExceptionThrow);
+        assertEquals("item", top);
+    }
+
+    // Test methods for pop() method
+
+    @Test
+    public void testPopEmpty() {
+        // setup
+        setUp1();
+        boolean isExceptionThrow = false;
+        int size = stack.size();
+
+        // act
+        try {
+            stack.pop();
+        }catch (StackException e){
+            isExceptionThrow = true;
+        }
+        // assert
+        assertTrue(isExceptionThrow);
+    }
+
+    @Test
+    public void testPopFilled() {
+        // setup
+        setUp1();
+        boolean isExceptionThrow = false;
+        String top = "";
+        int size = stack.size();
+
+        // act
+        try {
+            top = stack.pop();
+        }catch (StackException e){
+            isExceptionThrow = true;
+        }
+        // assert
+        assertFalse(isExceptionThrow);
+        assertEquals("item", top);
+        assertEquals(size-1, stack.size());
+    }
+}
