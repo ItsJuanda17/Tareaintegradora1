af50fdea277cc3a6d680b78a9c1f224e922924a6
Jose Manuel Cardona Rondón <124000426+manxi2004@users.noreply.github.com>
2023-10-14 19:20:55 -0500

    Update PriorityQueueTest.java

--- test/collections/PriorityQueueTest.java
+++ test/collections/PriorityQueueTest.java
@@ -4,6 +4,7 @@ import collections.dataStructures.PriorityQueue;
 import exception.QueueException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PriorityQueueTest {
@@ -14,58 +15,81 @@ public class PriorityQueueTest {
     void setUp() {
         priorityQueue = new PriorityQueue<>();
     }
+    @Test
+    public void testInsertAndExtractRoot() throws QueueException {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        // Insert values into the priority queue
+        priorityQueue.insert(5);
+        priorityQueue.insert(7);
+        priorityQueue.insert(3);
+        priorityQueue.insert(1);
+
+        // Extract the root (should be the largest value)
+        int extracted = priorityQueue.extractRoot();
+
+        assertEquals(7, extracted);
+    }
+
 
     @Test
-    void testInsertAndExtractRoot() throws QueueException {
-        // Insert elements into the priority queue
-        priorityQueue.insert(5);
-        priorityQueue.insert(3);
-        priorityQueue.insert(8);
+    public void testInsertAndExtractMultiple() throws QueueException {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        int[] values = {5, 7, 3, 1};
+        for (int value : values) {
+            priorityQueue.insert(value);
+        }
+        int[] expectedOrder = {7, 5, 3, 1};
+        for (int expected : expectedOrder) {
+            assertEquals(expected, priorityQueue.extractRoot());
+        }
+    }
 
-        // Extract elements and check if they are in the correct order
-        assertEquals(3, priorityQueue.extractRoot());
+
+
+    @Test
+    public void testSetKey() throws QueueException {
+
+        priorityQueue.insert(3);
+        priorityQueue.insert(5);
+        priorityQueue.insert(2);
+
+        priorityQueue.setKey(0, 5);
+
+
         assertEquals(5, priorityQueue.extractRoot());
-        assertEquals(8, priorityQueue.extractRoot());
-
-        // Ensure the priority queue is empty
-        assertTrue(priorityQueue.isEmpty());
-    }
-
-    @Test
-    void testResize() throws QueueException {
-        // Insert more elements than the initial capacity to trigger resizing
-        for (int i = 1; i <= 20; i++) {
-            priorityQueue.insert(i);
-        }
-
-        // Check if resizing worked and the elements are still in the correct order
-        for (int i = 1; i <= 20; i++) {
-            assertEquals(i, priorityQueue.extractRoot());
-        }
-
-        // Ensure the priority queue is empty
-        assertTrue(priorityQueue.isEmpty());
-    }
-
-    @Test
-    void testExceptionWhenExtractingFromEmptyQueue() {
-        assertThrows(QueueException.class, () -> priorityQueue.extractRoot());
-    }
-
-    @Test
-    void testSetKey() throws QueueException {
-        // Insert elements into the priority queue
-        priorityQueue.insert(5);
-        priorityQueue.insert(3);
-        priorityQueue.insert(8);
-
-        // Change the value of an element and check if it's still in the correct order
-        priorityQueue.setKey(1, 1);
-        assertEquals(1, priorityQueue.extractRoot());
         assertEquals(3, priorityQueue.extractRoot());
-        assertEquals(8, priorityQueue.extractRoot());
+        assertEquals(2, priorityQueue.extractRoot());
+    }
 
-        // Ensure the priority queue is empty
+
+    @Test
+    public void testGetRootWhenEmpty()  {
+        assertNull(priorityQueue.getRoot());
+    }
+
+
+    @Test
+    public void testExtractRootWhenEmpty() {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        // Try to extract the root when the priority queue is empty
+        assertThrows(QueueException.class, () -> {
+            priorityQueue.extractRoot();
+        });
+    }
+
+
+    @Test
+    public void testIsEmpty() throws QueueException {
+        assertTrue(priorityQueue.isEmpty());
+
+        priorityQueue.insert(1);
+
+        assertFalse(priorityQueue.isEmpty());
+
+        priorityQueue.extractRoot();
+
         assertTrue(priorityQueue.isEmpty());
     }
 }
